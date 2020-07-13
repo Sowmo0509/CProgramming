4803e87dcd154ceeb334e869a7b14ebb3fede093
Sowmo0509 <60974766+Sowmo0509@users.noreply.github.com>
2020-07-13 21:46:32 +0600

    Update for loop.c
    added the documentation

--- for loop.c
+++ for loop.c
@@ -1,5 +1,7 @@
-#include<stdio.h>
+/* This is a example of for loop in C. 
+It will print Sowmo for 5 times (0 to 5 = 1,2,3,4,5). */
 
+#include<stdio.h>
 
 int main(){
     for (int i = 0; i < 5; i++)
